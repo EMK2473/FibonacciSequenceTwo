c43ee29c16fa40d58c364c4bae5efacc943f0f78
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-19 11:52:51 -0400

    set up base case for negative values, and initial 0 and 1 values of fib seq; returned values for fib sequence to recursively call fibonacci method to calculate the values of each fib value; then add those values for the sum

--- FibonacciSequenceTwo.java
+++ FibonacciSequenceTwo.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class FibonacciSequenceTwo {
    
    public static int fibonacci(int n) {
-      /* Type your code here. */      
+      if(n < 0) {
+        return -1;
+      } 
+      else if (n == 0){
+        return 0;
+      } 
+      else if (n == 1) {
+        return 1;
+      }
+      return fibonacci(n - 1) + fibonacci(n -2);
    }
-   
+
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int startNum;
       
       startNum = scnr.nextInt();
       System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+      scnr.close();
    }
 }
